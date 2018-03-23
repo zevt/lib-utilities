02967dd2db399d4bb094900f4f68d0a717c2178c
Viet <vietsemantics@gmail.com>
2018-03-22 21:08:10 -0500

    init

---
+++ guava-demo/src/main/java/com/zeroexception/guava_demo/GuavaDemoApplication.java
@@ -0,0 +1,12 @@
+package com.zeroexception.guava_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuavaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuavaDemoApplication.class, args);
+	}
+}
---
+++ lombok-demo/src/main/java/com/zeroexception/lombok_demo/LombokDemoApplication.java
@@ -0,0 +1,12 @@
+package com.zeroexception.lombok_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LombokDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LombokDemoApplication.class, args);
+	}
+}
